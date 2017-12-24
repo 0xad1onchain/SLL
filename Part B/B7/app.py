8265f410b16c8fee9e0981d36ef2be6dd3c021b7
Aditya Gupta <aditya@Adityas-MacBook.local>
2017-12-25 02:06:14 +0530

    Added B7

---
+++ Part B/B7/app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df1 = pd.read_csv('titanic.csv')
+df1 = df1.drop(['Ticket'], 1)
+df1['Cabin'] = df1['Cabin'].fillna('NOPE')
+print('Number of Entries, Attributes:', df1.shape) 
+print(df1.keys()) #Print Attributes
+print(df1.dtypes) #Print Datatypes
+print('Max Age', df1['Age'].max())
+print('Min Age', df1['Age'].min())
+print('Mean Age', df1['Age'].mean())
+ax = df1['Age'].plot.hist()
+ax.set_xlabel('Age')
+ax.set_ylabel('Number of People')
+plt.show(block=True)
